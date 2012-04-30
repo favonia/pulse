19bcaba811fb833a7b1e1790d57716fe7e2d9a59
Favonia <favonia@gmail.com>
2012-04-29 23:27:22 -0400

    Test dummy `Monad.hs` in `Test.hs`

--- tests/Test.hs
+++ tests/Test.hs
@@ -4,4 +4,9 @@ module Main where
 import Sound.Pulse
 import Test.QuickCheck
 
-main = quickCheck ((==0).(`mod`2).(*2) :: Int -> Bool)
+main = do
+  runPulse DefaultServer DoNotWaitForDaemon code
+  runPulse DefaultServer WaitForDaemon code
+
+code :: Monad a => Pulse a ()
+code = return ()
